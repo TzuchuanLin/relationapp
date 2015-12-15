3e78c2e9f8f8ac8582e6d721d000caa0a4c79b92
TzuchuanLin <tlin103@syr.edu>
2015-12-15 11:51:21 -0500

    Create server.R

---
+++ server.R
@@ -0,0 +1,11 @@
+setwd( "/Users/tzuchuan/Desktop/shinyapp/relationapp" )
+LCdat<- read.csv("LCdat.csv", stringsAsFactors = F)
+library(shiny)
+  mat1<-as.data.frame.matrix(table(LCdat$Referred.To,LCdat$Referred.By))
+  server<-function(input,output,sessions) {(
+  output$Plot1<- renderPlot({
+    par(mar=c(5.1,8.1,4.1,2.1))
+    barplot(mat1[,input$mat1],main="Orgnizations receiving referrals",
+            xlim=c(0,7),ylim=c(-2,25), col="steel blue",names.arg=names(mat1),cex.names =0.8)
+   })
+  )}
