98328d6cd2277f7b912dc38ba087819ff27b4af3
TzuchuanLin <tlin103@syr.edu>
2015-12-15 12:35:17 -0500

    Update server.R

--- server.R
+++ server.R
@@ -1,4 +1,4 @@
-setwd( "/Users/tzuchuan/Desktop/relationapp" )
+setwd( "Macintosh HD/Users/tzuchuan/Desktop/relationapp" )
 LCdat<- read.csv("LCdat.csv", stringsAsFactors = F)
 library(shiny)
   mat1<-as.data.frame.matrix(table(LCdat$Referred.To,LCdat$Referred.By))
