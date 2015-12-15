f5c7837bbc4b12008702b129c60bb5577260329c
TzuchuanLin <tlin103@syr.edu>
2015-12-15 11:50:54 -0500

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,13 @@
+setwd( "/Users/tzuchuan/Desktop/shinyapp/relationapp" )
+LCdat<- read.csv("LCdat.csv", stringsAsFactors = F)
+library(shiny)
+mat1<-as.data.frame.matrix(table(LCdat$Referred.To,LCdat$Referred.By))
+shinyUI(fluidPage(
+  titlePanel(title=h4("The source of referral")),
+  sidebarPanel
+  (selectInput("mat1", "Select the orgnization that gives referral:",
+               choices=colnames(mat1))),
+  mainPanel(plotOutput("Plot1") )
+  
+)
+)
